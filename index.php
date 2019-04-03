c4141a828451978fec0cb04da6d82b21c37fbe63
Magnus Glantz <sudo@redhat.com>
2019-04-03 21:33:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  <head>
  </head>
  <body>
- <?php echo 'App Version: 18.0. Pod IP: '; ?>
+ <?php echo 'App Version: 19.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' PHP Version: ' . phpversion(); ?>
  </body>
