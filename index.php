1a07729f41d1c61457977c34a853098645028318
Håkan Hagenrud <hakan@redhat.com>
2019-08-29 09:49:29 +0200

    Smaller fontsize

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  </head>
  <body>
  <p><img src="https://www.php.net/images/logos/new-php-logo.png" alt="PHP logo">
- <font size="10">
+ <font size="6">
  <br><?php echo 'App Version: 2.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' Current PHP Version: ' . phpversion(); ?>
