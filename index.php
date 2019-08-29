5eb2d2a6e1015e5d8698b48374225f6bb071d8dd
Håkan Hagenrud <hakan@redhat.com>
2019-08-29 09:52:28 +0200

    Smaller image

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
  <title>PHP example app</title>
  </head>
  <body>
- <p><img src="https://www.php.net/images/logos/new-php-logo.png" alt="PHP logo">
+ <p><img src="https://www.php.net/images/logos/new-php-logo.png" alt="PHP logo" style="width:60px;">
  <font size="6">
  <br><?php echo 'App Version: 2.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
- <?php echo ' Current PHP Version: ' . phpversion(); ?>
+ <?php echo ' Current php Version: ' . phpversion(); ?>
  </font>
  </p>
  </body>
