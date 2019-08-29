a75315318bcd409b0703b4300a8beb19cf973650
Håkan Hagenrud <hakan@redhat.com>
2019-08-29 10:08:04 +0200

    Breaks where they should be

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  <body>
  <p><img src="https://www.php.net/images/logos/new-php-logo.png" alt="PHP logo">
  <font size="6">
- <br><?php echo 'App Version: 2.0. Pod IP: '; ?>
- <br><?php echo $_SERVER['SERVER_ADDR']; ?>
+ <br><?php echo 'App Version: 2.0.'; ?>
+ <br><?php echo 'Pod IP: ' + $_SERVER['SERVER_ADDR']; ?>
  <br><?php echo ' Current php Version: ' . phpversion(); ?>
  </font>
  </p>
