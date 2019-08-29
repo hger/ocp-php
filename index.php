b74d06b9cf8d3d0f9c40b0b1b6c126b63276f30e
Håkan Hagenrud <hakan@redhat.com>
2019-08-29 09:07:50 +0200

    Now with font sizes

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
  <head>
  </head>
  <body>
+ <font size="18">
  <?php echo 'App Version: 2.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' Current PHP Version: ' . phpversion(); ?>
+ </font>
  </body>
 </html>
