f9b067bfdbff7187c14928f48830ccc117fbef8a
Håkan Hagenrud <hakan@redhat.com>
2019-08-29 09:16:20 +0200

    image and heading

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <html>
  <head>
+ <title>PHP example app</title>
  </head>
  <body>
- <font size="18">
+ <img src="https://www.php.net/images/logos/new-php-logo.png" alt="PHP logo">
+ <font size="12">
  <?php echo 'App Version: 2.0. Pod IP: '; ?>
  <?php echo $_SERVER['SERVER_ADDR']; ?>
  <?php echo ' Current PHP Version: ' . phpversion(); ?>
