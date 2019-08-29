89ad19b945c4fd1e0f9d1ef6d4957341d9abd86a
Håkan Hagenrud <hakan@redhat.com>
2019-08-29 10:11:53 +0200

    Breaks again

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  <p><img src="https://www.php.net/images/logos/new-php-logo.png" alt="PHP logo">
  <font size="6">
  <br><?php echo 'App Version: 2.0.'; ?>
- <br><?php echo 'Pod IP: ' + $_SERVER['SERVER_ADDR']; ?>
+ <br><?php echo 'Pod IP: '; ?><?php echo $_SERVER['SERVER_ADDR']; ?>
  <br><?php echo ' Current php Version: ' . phpversion(); ?>
  </font>
  </p>
